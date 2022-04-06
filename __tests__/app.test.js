669fda20e78243184b8ea467352091d251cf60ef
Philippe Ngom <philngom@gmail.com>
2022-04-06 13:36:17 -0700

    wrote first test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,6 +3,8 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+jest.mock('../lib/utils/__mocks__/github');
+
 describe('gitty routes', () => {
   beforeEach(() => {
     return setup(pool);
@@ -10,5 +12,13 @@ describe('gitty routes', () => {
 
   afterAll(() => {
     pool.end();
+  });
+
+  it('should redirect to github oauth page upon login', async () => {
+    const res = request(app).get('/api/v1/github/login');
+
+    expect(res.header.location).toMatch(
+      /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/login\/callback/i
+    );
   });
 });
