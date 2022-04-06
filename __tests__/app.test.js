0d3736e9fb1e488dba755616c6ad5dc8087fb8bb
Philippe Ngom <philngom@gmail.com>
2022-04-06 15:01:29 -0700

    wrote login route, test 1 passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -15,7 +15,8 @@ describe('gitty routes', () => {
   });
 
   it('should redirect to github oauth page upon login', async () => {
-    const res = request(app).get('/api/v1/github/login');
+    const res = await request(app).get('/api/v1/github/login');
+    console.log('header', res.header.location);
 
     expect(res.header.location).toMatch(
       /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/login\/callback/i
