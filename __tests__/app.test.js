a461d048c539bac5246379ad15cccfe6d0457f4a
Philippe Ngom <philngom@gmail.com>
2022-04-06 16:25:36 -0700

    wrote test #2

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -16,10 +16,24 @@ describe('gitty routes', () => {
 
   it('should redirect to github oauth page upon login', async () => {
     const res = await request(app).get('/api/v1/github/login');
-    console.log('header', res.header.location);
 
     expect(res.header.location).toMatch(
       /https:\/\/github.com\/login\/oauth\/authorize\?client_id=[\w\d]+&scope=user&redirect_uri=http:\/\/localhost:7890\/api\/v1\/github\/login\/callback/i
     );
+  });
+
+  it('should login and redirect users to /api/v1/github/dashboard', async () => {
+    const res = await request.agent(app)
+      .get('/api/v1/github/login/callback?code=42')
+      .redirects(1);
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      username: 'fake_github_user',
+      email: 'not-real@example.com',
+      avatar: expect.any(String),
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+    });
   });
 });
