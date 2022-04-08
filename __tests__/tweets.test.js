90de23c56f83a542a111be4ae144f2d2275719ed
Philippe Ngom <philngom@gmail.com>
2022-04-08 12:24:38 -0700

    wrote test for quote get route

--- __tests__/tweets.test.js
+++ __tests__/tweets.test.js
@@ -2,11 +2,9 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const GithubUser = require('../lib/models/GithubUser');
 const Tweet = require('../lib/models/Tweet');
 
 jest.mock('../lib/utils/github');
-// jest.mock('../lib/middleware/authenticate');
 
 describe('gitty routes', () => {
   beforeEach(() => {
@@ -18,11 +16,6 @@ describe('gitty routes', () => {
   });
 
   it('should allow user to post tweets', async () => {
-    // await GithubUser.insert({
-    //   username: 'fake_github_user',
-    //   avatar: 'https://www.placecage.com/gif/300/300',
-    //   email: 'not-real@example.com',
-    // });
     const agent = request.agent(app);
     await agent
       .get('/api/v1/github/login/callback?code=42')
