9459d101121150dda74f4383f0aedd5f24ffb851
Philippe Ngom <philngom@gmail.com>
2022-04-07 23:27:04 -0700

    wrote test to get all posts from users

---
+++ __tests__/tweets.test.js
@@ -0,0 +1,34 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+jest.mock('../lib/utils/github');
+
+describe('gitty routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('should get all posts from all users', async () => {
+    const res = await request(app).get('/api/v1/posts');
+
+    expect(res.body).toEqual([
+      {
+        id: expect.any(String),
+        tweet: 'this is my first tweet',
+        username: 1
+      },
+      {
+        id: expect.any(String),
+        tweet: 'this is my second tweet',
+        username: 2
+      }
+    ]);
+
+  });
+});
